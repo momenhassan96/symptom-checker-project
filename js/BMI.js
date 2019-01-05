042a140d21384220f1b114f3afa93635a5eff944
momenhassan <momenhassan1996@gmail.com>
2019-01-05 03:12:43 +0200

    Update cslori , heart rate and BMI pages

--- js/BMI.js
+++ js/BMI.js
@@ -2,16 +2,12 @@ $(window).scroll(function(){
         var scrTop= $(window).scrollTop();
         if(scrTop>150)
             {
-                $(".navbar").css("background-color","#13c5dd");
-                $(".nav-link").css("color","#fff");
-                $(".navbar-brand").css("color","#fff");
+                 $(".navbar").css("background-color","rgba(255, 255, 255, 0.5)");
             }
      
         else 
             {
-                  $(".navbar").css("background-color","#fff");
-                $(".nav-link").css("color","#13c5dd");
-                $(".navbar-brand").css("color","#13c5dd");
+                 $(".navbar").css("background-color","#fff");
             }
      
      if(scrTop<150)
